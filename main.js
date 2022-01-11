7c3787b23370f981f68c381f2dbeda98bc8c5292
Вадим <64354618+VadimZakharchenko@users.noreply.github.com>
2022-01-11 14:35:03 +0300

    Added modal

--- main.js
+++ main.js
@@ -40,3 +40,41 @@ function clickHandler(e) {
                document.getElementById('btnUp').className = 'button-up hidden';
            }
        }
+// POPUP
+let popupBg = document.querySelector('.popup-bg'); // Фон попап окна
+let popup = document.querySelector('.popup'); // Само окно
+let openPopupButtons = document.querySelectorAll('.events-link-notv'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
+
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+        popup.classList.add('active'); // И для самого окна
+    })
+});
+
+closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
+    popupBg.classList.remove('active'); // Убираем активный класс с фона
+    popup.classList.remove('active'); // И с окна
+});
+
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+    if(e.target === popupBg) { // Если цель клика - фон, то:
+        popupBg.classList.remove('active'); // Убираем активный класс с фона
+        popup.classList.remove('active'); // И с окна
+}
+});
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const inputElement = document.querySelector('input') // ищем наш единственный input
+    const maskOptions = { // создаем объект параметров
+      mask: '+{7}(000)000-00-00' // задаем единственный параметр mask
+    }
+    IMask(inputElement, maskOptions) // запускаем плагин с переданными параметрами
+  
+  })
+  
